809550b7cc2d2088be742bf333e20c12d381c35f
haakooto <hakonolav.torvik@gmail.com>
2021-09-02 15:24:13 +0200

    add plot for v

--- Project1/plot.py
+++ Project1/plot.py
@@ -4,7 +4,7 @@ import pandas as pd
 file = "data.txt"
 data = pd.read_csv(file, header=0, sep=" ")
 
-fig = px.line(data, x="x", y="u", title="Analytical solutions")
+fig = px.line(data, x="x", y=["u", "v"], title="Analytical solutions")
 fig.update_layout(
     font_family="Garamond",
     font_size=25,
