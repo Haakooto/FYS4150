f2693de2064077480e8a4218fa09176c59ba2262
haakooto <hakonolav.torvik@gmail.com>
2021-09-09 23:11:46 +0200

    add comments

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -9,14 +9,15 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-    int n;
-    int m;
+    int n;  // largest log(N) to run algorithm for
+    int m;  // number of repetitions for each n
     n = atoi(argv[1]);
-    m = atoi(argv[2]);   //repetitions
+    m = atoi(argv[2]);
 
     // To not wait forever for writing full output, cap such writing for low n
     int cap = 4;
 
+    // Choosing algorithm from command line
     void (*Method)(int, int, int, int, double*, double*) = NULL;
     string name;
     if (argc == 4){
@@ -26,15 +27,18 @@ int main(int argc, char *argv[]){
         name = "";
         Method = Thomas;
     }
+
     int ns[m * n];
-    double mres[m * n];
+    double mres[m * n];  // mre = max relative error
     double times[m * n];
+
     for (int i = 1; i < n + 1; i++){
         for (int j = 0; j < m; j++){
             cout << "starting " + name + "Thomas algorithm for n = " << i << endl;
             Method(i, j, m, cap, times, mres);
             ns[(i - 1) * m + j] = i;
-     }}
+        }
+    }
     write_limited(n, m, ns, times, mres, name);
     return 0;
 }
