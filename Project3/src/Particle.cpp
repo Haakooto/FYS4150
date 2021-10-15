c82b84f44fba083859d4d2c16be706282de17716
haakooto <hakonolav.torvik@gmail.com>
2021-10-15 15:13:31 +0200

    move stuff to own files

--- Project3/src/Particle.cpp
+++ Project3/src/Particle.cpp
@@ -1,12 +1,14 @@
 #include "Particle.hpp"
 
+
 Particle::Particle(arma::vec position, arma::vec velocity, double mass, int charge){
 		r = position;
 		v = velocity;
 		m = mass;
 		q = charge;
 }
-Particle::print(){
+
+void Particle::print(){
 		cout << "Particle at ( ";
 		for (int i=0; i<3; i++){cout << r(i) << " ";}
 		cout << ") with velocity ( ";
