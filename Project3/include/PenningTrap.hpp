8fe398aed834d0f7b7c9bb2f0309daa03068dde0
haakooto <hakonolav.torvik@gmail.com>
2021-10-18 12:25:08 +0200

    add comments

--- Project3/include/PenningTrap.hpp
+++ Project3/include/PenningTrap.hpp
@@ -3,32 +3,37 @@
 
 #include <armadillo>
 #include <vector>
+#include <complex>
 
 class PenningTrap
 /*
 */
 {
 public:
-    int N=0;
+    int N=0;  // number of particles in trap
     PenningTrap(double Bfield, double Efield, double length, bool particle_particle=false );
     PenningTrap(double Bfield, double(*)(double), double length, bool particle_particle=false );
+    // Overloaded insertion methods
     void insert_particles(vector<Particle>);
     void insert_particles(Particle);
     void insert_particles(int, double, int);
     void simulate(double, double);
     void analytic(double, double, double, double, double);
+    arma::cube get_history();  // returns r
+    arma::vec get_time();  // retruns t
+    int escaped();  // counts particles left in trap
 
 private:
-    int nT;
-    double dt;
-    double B0, V0, d;
-    double (*tV0)(double);
-    vector<Particle> particles;
+    int nT;  // number of time steps
+    double dt;  // length of time step
+    double B0, V0, d;  // properties of trap
+    double (*tV0)(double);  // time-dep E-field
+    vector<Particle> particles;  // Particle container
     bool ppi, time_dep_V;
-    arma::cube r;
-    arma::mat v;
-    arma::vec t;
-    arma::rowvec Q, M;
+    arma::cube r;  // postion of all particles at all times
+    arma::mat v;  // velocity of all particles, only one time step
+    arma::vec t;  // time vector
+    arma::rowvec Q, M;  // properties of particles
 
     double get_Efield_at_time(double);
     arma::mat sum_particle_forces(arma::mat);
@@ -37,7 +42,6 @@ private:
     arma::cube advance(double, arma::cube);
     void Efield(arma::mat &, double );
     void Bfield(arma::mat &);
-
 
 };
 
