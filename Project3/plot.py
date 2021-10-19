748f381ce17a77f1d8ce8a7ed9962399990b4f89
haakooto <hakonolav.torvik@gmail.com>
2021-10-19 17:40:14 +0200

    plots

--- Project3/plot.py
+++ Project3/plot.py
@@ -4,6 +4,24 @@ import plotly.graph_objects as go
 import pandas as pd
 
 
+def make_nice_plot(fig, xlabel="", ylabel="", title="", legend=None):
+    layout = go.Layout(
+        font_family="Garamond",
+        font_size=35,
+        xaxis=dict(title=xlabel),
+        yaxis=dict(title=ylabel),
+        title=title,
+    )
+    # fig.update_layout(font_family="Garamond",
+    #                   font_size=35,
+    #                   title=title,
+    #                   xaxis_title=xlabel,
+    #                   yaxis_title=ylabel,
+    #                   )
+    # if legend is not None:
+        # fig.update_layout(legend=legend)
+    return layout
+
 def main():
     df = pd.read_csv("outputs/oneP_endurace.txt", header=0, sep=" ")
     print(df)
@@ -17,10 +35,23 @@ def main():
                         )
     fig.show()
 
-def plot_z():
+
+def ex9_plot_z():
     df = pd.read_csv("outputs/oneP_endurance.txt", header=0, sep=" ")
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=df["time"], y=df["z"], mode="lines"))
+    N = df.shape[0]
+
+    dt = df["time"][1]
+    yf = np.fft.rfft(df["z"])
+    xf = np.fft.rfftfreq(N, d=dt)
+
+    wz = np.sqrt(2 * 9.65 / 40.078)
+    freq = 2 * np.pi * xf[np.argmax(np.abs(yf))]
+    print(1 - freq / wz)  # relative error in freq
+
+    trace = go.Scatter(x=df["time"], y=df["z"], mode="lines")
+    lo = make_nice_plot(r"$Time [\mu s]$", r"$z [\mu m]$",
+                   r"$\text{Movement in z-direction for single particle}$")
+    fig = go.Figure(data=[trace,], layout=lo)
     fig.show()
 
 
@@ -30,12 +61,14 @@ def plot_xy_plane():
         df = pd.read_csv(f"outputs/twoP{b}_ppi.txt", header=0, sep=" ")
         for i in range(1, 2):
             dP = df.loc[lambda x: x["particle"] == i, :]
-            fig.add_trace(go.Scatter(x=dP["x"], y=dP["y"], mode="lines", name=f"particle {i} {b} ppi"))
+            fig.add_trace(go.Scatter(
+                x=dP["x"], y=dP["y"], mode="lines", name=f"particle {i} {b} ppi"))
 
-    fig.update_layout(xaxis_range=[-30, 30], yaxis_range=[-30,30])
+    fig.update_layout(xaxis_range=[-30, 30], yaxis_range=[-30, 30])
     # fig = px.scatter(df, x="x", y="y", animation_frame="time", animation_group="particle", range_y=[-20, 20])
     fig.show()
 
+
 if __name__ == "__main__":
     # main()
-    plot_xy_plane()
+    ex9_plot_z()
