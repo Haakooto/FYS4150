5fb3918641e22f230ba6b7318eea6bd4dfd57fce
haakooto <hakonolav.torvik@gmail.com>
2021-10-19 15:17:16 +0200

    do more plot

--- Project3/plot.py
+++ Project3/plot.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 
 def main():
-    df = pd.read_csv("test.txt", header=0, sep=" ")
+    df = pd.read_csv("outputs/oneP_endurace.txt", header=0, sep=" ")
     print(df)
     time0 = df.loc[lambda df: df["time"] == 0, :]
     # fig = go.Figure(
@@ -17,6 +17,25 @@ def main():
                         )
     fig.show()
 
+def plot_z():
+    df = pd.read_csv("outputs/oneP_endurance.txt", header=0, sep=" ")
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df["time"], y=df["z"], mode="lines"))
+    fig.show()
+
+
+def plot_xy_plane():
+    fig = go.Figure()
+    for b in ["", "_no"]:
+        df = pd.read_csv(f"outputs/twoP{b}_ppi.txt", header=0, sep=" ")
+        for i in range(1, 2):
+            dP = df.loc[lambda x: x["particle"] == i, :]
+            fig.add_trace(go.Scatter(x=dP["x"], y=dP["y"], mode="lines", name=f"particle {i} {b} ppi"))
+
+    fig.update_layout(xaxis_range=[-30, 30], yaxis_range=[-30,30])
+    # fig = px.scatter(df, x="x", y="y", animation_frame="time", animation_group="particle", range_y=[-20, 20])
+    fig.show()
 
 if __name__ == "__main__":
-    main()
+    # main()
+    plot_xy_plane()
