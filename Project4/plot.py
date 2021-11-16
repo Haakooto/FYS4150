25e6eed43c6d729f44c8008593d0e162c63bbd80
haakooto <hakonolav.torvik@gmail.com>
2021-11-16 18:23:49 +0100

    sart write plot func

--- Project4/plot.py
+++ Project4/plot.py
@@ -1,27 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
-import os
-import time
+from glob import glob
+import subprocess
 
+datapath = "./data/"
 
-def burntime(fname, *args):
-    pass
+def burntime(fname, M, new_run=False):
+    M = int(M)
+    if fname in glob(datapath):
+        pass
 
 
 def main():
-    if len(sys.argv) == 1:
-        return
-    elif len(sys.argv) > 2:
-        args = [a for a in sys.argv[2:]]
-    else:
-        args = None
+    if "help" in sys.argv:
+        print("First argument after 'python plot.py' must be a function in this file")
+        print("If this function takes any arguments, the are to be passed as well")
+        print("Example: 'python plot.py burntime burn 10")
 
-    func = sys.argv[1]
-    if args is not None:
-        exec(func + f"(*{args})")
-    else:
-        exec(func + "()")
+    try:
+        if len(sys.argv) == 1:
+
+            return
+        elif len(sys.argv) > 2:
+            args = [a for a in sys.argv[2:]]
+        else:
+            args = None
+
+        func = sys.argv[1]
+        if args is not None:
+            exec(func + f"(*{args})")
+        else:
+            exec(func + "()")
+    except:
+        print("Bad usage! Pass 'help' from commandline to see guide")
 
 if __name__ == "__main__":
     main()
