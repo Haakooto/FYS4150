0fbb05a7e4ec52c3a06a196422d918e5a23e9c5d
haakooto <hakonolav.torvik@gmail.com>
2021-11-18 02:22:33 +0100

    update to new system

--- Project4/pdf.cpp
+++ Project4/pdf.cpp
@@ -11,29 +11,32 @@
 using namespace std;
 using namespace arma;
 
+
 int main(int argc, char* argv[]){
-    
+
     string fname1, fname2;
-    int M;
-	if (argc != 4){
-		std::cout << "Bad usage! This program takes three params";
-		std::cout << "\nfilename1, filename2, and number of monte carlo cycles\n";
+    int M, R, L=20;
+    int burnin = 0;
+	if (argc != 5){
+		std::cout << "Bad usage! This program takes four params";
+		std::cout << "\nfilename1, filename2, number of monte carlo cycles, and number of cocurrent mcc\n";
 		return 1;
 	}else{
 		fname1 = argv[1];
         fname2 = argv[2];
         M = atoi(argv[3]);
+        R = atoi(argv[4]);
     }
+
     ofstream out1, out2;
     out1.open("data/" + fname1 + ".csv");
     out2.open("data/" + fname2 + ".csv");
-    
-    mat Lattice = make_sys(20, "random");
-    mat Prob = mc_run(Lattice, 1., M);
-    Prob.save(out1, raw_ascii);
-    
-    Prob = mc_e_prob(Lattice, 2.4, M);
-    Prob.save(out2, raw_ascii);
+
+    mat Prob = multi_prob(L, M, R, 1, burnin);
+    Prob.save(out1, csv_ascii);
+
+    Prob = multi_prob(L, M, R, 2.4, burnin);
+    Prob.save(out2, csv_ascii);
 
     return 0;
 }
