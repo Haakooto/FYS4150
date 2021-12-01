ec67727c2ec0554405fb018bca76b949fa40c1e0
haakooto <hakonolav.torvik@gmail.com>
2021-12-01 13:19:19 +0100

    do all the commenting

--- Project5/Crank_Nicolson.jl
+++ Project5/Crank_Nicolson.jl
@@ -3,12 +3,29 @@ using Random
 using SparseArrays
 # using Pkg
 # Pkg.add("NPZ")
+# Pkg.add("ProgressMeter")
 using NPZ
+using ProgressMeter
 
 include("sparse_matrices.jl")
 
-function make_AB(M, h, dt, V)
-    m = M - 2
+function make_AB(h, dt, V)
+    """
+    Sets up the sparse matrices A and B,
+    with diagonals a and b, r as non-diagonal elements
+
+    Arguments:
+        h: Float
+            Step length in spatial discretisation
+        dt: Float
+            Step length in temporal discretisation
+        V: Matrix{Float}
+            Matrix of potential values in each point
+    Returns 
+        A, B: SparseMat
+            Sparse matrices, as defined in assignment
+    """
+    m = Int(1 / h) - 2
     a = ones(ComplexF64, m*m)
     b = ones(ComplexF64, m*m)
     r = 1im * dt / 2  # defined without the h^2
@@ -20,14 +37,45 @@ function make_AB(M, h, dt, V)
     return SparseMat(a, -r), SparseMat(b, r)
 end
 
-function make_V(M, h, v0, slits=2)
-    m = M - 2
+function make_V(h, v0, slits)
+    """
+    Sets up potential matrix V
+    Must still be written, currently no potential is used
+
+    Arguments:
+        h: Float
+            Step length in spatial discretisation
+        v0: Float
+            Value of potential where not zero
+        slits: Int
+            Number of slits in the slit experiment
+    Returns:
+        V: Matrix{Float}
+            Potential matrix
+    """
+    m = Int(1 / h) - 2
     V = zeros(m, m)
     return V
 end
 
-function Gauss_wave_packet(M, h, xc, yc, px, py, sx, sy)
-    x = [i * h for i in 0:M-1]
+function Gauss_wave_packet(h, xc, yc, px, py, sx, sy)
+    """
+    Initializes the system with a 2D gaussian wave packet
+
+    Arguments:
+        h: Float
+            Step length in spatial discretisation 
+        xc, yc: Float
+          Centre of wave packet in x and y coordinate
+        px, py: Float
+            wave packet momentum in x and y coordinate
+        sx, sy: Float
+            Initial width of wave packet in x and y coordinate
+    Returns
+        u:  Array{ComplexF}
+            Normalised, flattened array of initial wave packet
+    """
+    x = [i * h for i in 0:Int(1/h)-1]
     X, Y = meshgrid(x, x)
 
     t1x = -((X .- xc).^2) ./ (2 * sx^2)
@@ -38,21 +86,44 @@ function Gauss_wave_packet(M, h, xc, yc, px, py, sx, sy)
     u = exp.(t1x .+ t1y .+ t2x .+ t2y)
 
     u /= sqrt(sum(abs2.(u)))  # normalize u
+    u = vec(u[2:end-1, 2:end-1])  # Flatten, chop off boundaries
     return u
 end
 
-function initialize_system(M, dt, h, v0, P)
-    V = make_V(M, h, v0)
-    u = Gauss_wave_packet(M, h, P...)
-    A, B = make_AB(M, h, dt, V)
+function initialize_system(dt, h, v0, slits, P)
+    """
+    Wrapper, initializes the system
+
+    Arguments
+        dt: Float
+            Step length in temporal discretisation
+        h: Float
+            Step length in spatial discretisation
+        v0: Float
+            Value of potential where not zero
+        slits: Int
+            number of slits in the slit experiment
+        P: Array{Float}
+            Container for parameters to gaussian wave packet
+    Returns:
+        A, B: SparseMat
+            A and B as specified in assigmnet
+        u: Array{ComplexF}
+            Normalized, flattened complex array of initial wave packet
+    """
+    V = make_V(h, v0, slits)
+    u = Gauss_wave_packet(h, P...)
+    A, B = make_AB(h, dt, V)
     return A, B, u
 end
 
 function main()
+    # Discretisation free parameters
     h = 0.005
     dt = 2.5e-5
     T = 0.008
 
+    # System free parameters
     xc = 0.25
     sx = 0.005
     px = 200
@@ -60,26 +131,27 @@ function main()
     sy = 0.05
     py = 0
     v0 = 0
+    slits = 2
 
+    # Dependent parameters
     gauss_params = [xc, yc, px, py, sx, sy]
-
     M = Int(1 / h)
     nT = Int(T / dt)
 
-    A, B, u = initialize_system(M, dt, h, v0, gauss_params)
-    u = vec(u[2:end-1, 2:end-1])
+    A, B, u = initialize_system(dt, h, v0, slits, gauss_params)
 
+    # Probs contains the probability at all points, at all times, as well as time array
+    # Must be changed, depending on what we want show
     probs = zeros(Float64, nT, (M - 2)^2 + 1)
     probs[:, 1] = collect(range(0, T, nT))
     probs[1, 2:end] = abs2.(u)
 
-    for i in 2:nT
-        print("time iteration i = $i\u001b[1000D")
-        b = B * u
-        u, _ = SOR(A, b, initial_guess=u, omega=1, tol=1e-13)
-        probs[i, 2:end] = abs2.(u)
+    @showprogress "Iterating over time:" for i in 2:nT
+        b = B * u  # Problem 3 step 1
+        u, _ = SOR(A, b, initial_guess=u, omega=1, tol=1e-13)  # Problem 3 step 2
+        probs[i, 2:end] = abs2.(u)  # record
     end
-    println(sum(abs2.(u)))
+    println(sum(abs2.(u)))  # final total probability
     npzwrite("evolve3.npz", probs)
 end
 
--- Project5/sparse_matrices.jl
+++ Project5/sparse_matrices.jl
@@ -2,29 +2,42 @@ using LinearAlgebra
 import Base.:*
 
 struct SparseMat
+    """
+    Because the shape of A and B, 
+    only need to store the diagonal and off-diag element r 
+    """
     diag::Array
     r::ComplexF64
     m::Int
 end
 
-SparseMat(d, r) = SparseMat(d, r, Int(sqrt(length(d))))
+SparseMat(d, r) = SparseMat(d, r, Int(sqrt(length(d))))  # Functor ('constructor function')
 
-function get_at(A::SparseMat, i::Int, j::Int)
+function Base.getindex(A::SparseMat, i::Int, j::Int)
+    """
+    For indexing SparseMat
+    Returns matrix element at index (i, j)
+    Used as normal matrix indexing, A[i, j]
+    """
     if i <= 0 || j <= 0 || i > A.m^2 || j > A.m^2
-        return 0
+        return 0  # outside of matrix
     else
-        if i == j
+        if i == j  # diagonal
             return A.diag[i]
-        elseif abs(i - j) == 1
-            t = min(i, j)
+        elseif abs(i - j) == 1  # first off-diagonal is r, except every mth element
+            t = min(i, j)  # to go down or left
             return (mod(t, A.m) != 0) * A.r
-        else
-            return (abs(i - j) == A.m) * A.r
+        else  # mth off-diagonal is r
+            return (abs(i - j) == A.m) * A.r 
         end
     end
 end
 
 function over_index(a::Array, i::Int)
+    """
+    To not deal with pesky indexing errors, 
+    let indexing outside of array return 0
+    """
     if i <=0 || i > length(a)
         return 0
     else
@@ -33,23 +46,31 @@ function over_index(a::Array, i::Int)
 end
 
 function to_dense(SM::SparseMat)
+    """
+    Convert a SparseMat to dense
+    Must be done to print sparse matrix
+    Loops over all indexes and fills empty matrix
+    """
     n = SM.m^2
     out = zeros(ComplexF64, n, n)
     for i in 1:n
         for j in 1:n
-            out[i, j] = get_at(SM, i, j)
+            out[i, j] = SM[i, j]
         end
     end
     return out
 end
 
 function *(A::SparseMat, x::Array)
+    """
+    Performs matrix multiplication between SparseMat and Array
+    Uses the fact that A is sparse to only perform nessisary calculations
+    """
     b = zeros(ComplexF64, length(x))
-    # b = zeros(typeof(x[1]), size(x))
-    indices = [-A.m, -1, 0, 1, A.m]
+    indices = [-A.m, -1, 0, 1, A.m]  # non-zero elements of A
     for i in 1:A.m^2
         for j in indices
-            b[i] += get_at(A, i, i + j) * over_index(x, i + j)
+            b[i] += A[i, i + j] * over_index(x, i + j)
         end
     end
     return b
@@ -70,7 +91,7 @@ function SOR(A::SparseMat, b::Array; initial_guess=0, omega=1.5, tol=1e-13)
         for i in 1:n
             Sigma = 0  # sum(Aij * xj)
             for j in indices
-                Sigma += get_at(A, i, j + i) * over_index(x, i + j)
+                Sigma += A[i, j + i] * over_index(x, i + j)
             end
             x[i] = (1 - omega) * x[i] + (omega / A.diag[i]) * (b[i] - Sigma)
         end
@@ -83,6 +104,9 @@ function SOR(A::SparseMat, b::Array; initial_guess=0, omega=1.5, tol=1e-13)
 end
 
 function meshgrid(x, y)
+    """
+    return a meshgrid of x and y
+    """
     X = [i for i in x, j in 1:length(y)]
     Y = [j for i in 1:length(x), j in y]
     return X, Y
