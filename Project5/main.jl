242be391c0391844cd6b9338e42a1d21d4671060
haakooto <hakonolav.torvik@gmail.com>
2021-12-09 17:41:36 +0100

    simplify code

--- Project5/main.jl
+++ Project5/main.jl
@@ -21,12 +21,11 @@ function problem7_no_slit()
     sy = 0.05
     v0 = 0
     slits = 0
-    save_also_real_and_imag_part = false
 
     # The 7 other system parameters should not be changed
     args = [0.005, 2.5e-5, T, 0.25, 0.5, 200, 0, 0.05, sy, v0, slits]
     name = "p7_no_slit"
-    simulate(args, name, save_also_real_and_imag_part)
+    simulate(args, name)
 end
 
 function problem7_double_slit()
@@ -35,12 +34,11 @@ function problem7_double_slit()
     sy = 0.10
     v0 = 1e10
     slits = 2
-    save_also_real_and_imag_part = false
 
     # The 7 other system parameters should not be changed
     args = [0.005, 2.5e-5, T, 0.25, 0.5, 200, 0, 0.05, sy, v0, slits]
     name = "p7_double_slit"
-    simulate(args, name, save_also_real_and_imag_part)
+    simulate(args, name)
 end
 
 function problem8()
@@ -49,12 +47,13 @@ function problem8()
     sy = 0.20
     v0 = 1e10
     slits = 2
-    save_also_real_and_imag_part = true
+
+    realimag = true
 
     # The 7 other system parameters should not be changed
     args = [0.005, 2.5e-5, T, 0.25, 0.5, 200, 0, 0.05, sy, v0, slits]
     name = "p8"
-    simulate(args, name, save_also_real_and_imag_part)
+    simulate(args, name, realimag)
 end
 
 function problem9_one_slit()
@@ -63,12 +62,11 @@ function problem9_one_slit()
     sy = 0.20
     v0 = 1e10
     slits = 1
-    save_also_real_and_imag_part = false
 
     # The 7 other system parameters should not be changed
     args = [0.005, 2.5e-5, T, 0.25, 0.5, 200, 0, 0.05, sy, v0, slits]
     name = "p9_one_slit"
-    simulate(args, name, save_also_real_and_imag_part)
+    simulate(args, name)
 end
 
 function problem9_two_slit()
@@ -77,12 +75,11 @@ function problem9_two_slit()
     sy = 0.20
     v0 = 1e10
     slits = 2
-    save_also_real_and_imag_part = false
 
     # The 7 other system parameters should not be changed
     args = [0.005, 2.5e-5, T, 0.25, 0.5, 200, 0, 0.05, sy, v0, slits]
     name = "p9_two_slit"
-    simulate(args, name, save_also_real_and_imag_part)
+    simulate(args, name)
 end
 
 function problem9_three_slit()
@@ -91,20 +88,19 @@ function problem9_three_slit()
     sy = 0.20
     v0 = 1e10
     slits = 3
-    save_also_real_and_imag_part = false
 
     # The 7 other system parameters should not be changed
     args = [0.005, 2.5e-5, T, 0.25, 0.5, 200, 0, 0.05, sy, v0, slits]
     name = "p9_three_slit"
-    simulate(args, name, save_also_real_and_imag_part)
+    simulate(args, name)
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
     # potential is not made, only this one works
-    problem7_no_slit()
-    problem7_double_slit()
+    # problem7_no_slit()
+    # problem7_double_slit()
     problem8()
-    problem9_one_slit()
-    problem9_two_slit()
-    problem9_three_slit()
+    # problem9_one_slit()
+    # problem9_two_slit()
+    # problem9_three_slit()
 end
