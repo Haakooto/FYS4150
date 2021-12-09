c7f5515a112481f350db57098f1793d241f7f4d9
SaraPJensen <sarapje@ifi.uio.no>
2021-12-09 12:18:20 +0100

    finished plotting

--- Project5/animate.py
+++ Project5/animate.py
@@ -2,19 +2,28 @@ import numpy as np
 import plotly.graph_objects as go
 
 
-def animate():
+def position_to_index(position, h = 0.005):
+    index = position/h + 1
+    return int(index)
+
+def time_to_index(time, step = 2.5e-5):
+    index = time/step
+    return int(index)
+
+
+def animate(h = 0.005):
     data = np.load("npz/p7_triple_slit.npz")
     m = int(np.sqrt(data.shape[1] - 1))
     t = data[:, 0]
     Z = data[:, 2:].reshape(len(t), m, m)
-    x = np.linspace(0, 1, m)
+    axes = np.linspace(h, 1-h, m)
 
     data = go.Contour(x=x, y=x,
                       z=Z[0],
                       #   contours=dict(start=0, end=0.1, size=0.01, showlines=False),
                       )
 
-    frames = [go.Frame(data=[go.Contour(x=x, y=x, z=Z[i].reshape(m, m))])
+    frames = [go.Frame(data=[go.Contour(x=axes, y=axes, z=Z[i].reshape(m, m))])
               for i in range(1, len(t))]
 
     layout = go.Layout(updatemenus=[dict(type="buttons", buttons=[
@@ -24,18 +33,22 @@ def animate():
     f.show()
 
 
-def plot_states():
-    prob = np.load("npz/prob.npz")
+
+def plot_states(h = 0.005):
+    #prob = np.load("npz/prob.npz")
+    prob = np.load("npz/p7_no_slit_final.npz")
     m = int(np.sqrt(prob.shape[1] - 1))
     t = prob[:, 0]
     p = prob[:, 2:].reshape(len(t), m, m)
-    x = np.linspace(0, 1, m)
+
+    x = np.linspace(h, 1-h, m)
     fig = go.Figure(data=go.Contour(x=x, y=x, z=p[np.argmin(abs(t - 0))]))
     fig.show()
     fig = go.Figure(data=go.Contour(x=x, y=x, z=p[np.argmin(abs(t - 0.004))]))
     fig.show()
     fig = go.Figure(data=go.Contour(x=x, y=x, z=p[np.argmin(abs(t - 0.008))]))
     fig.show()
+
 
 
 def plot_initial_u():
@@ -69,7 +82,7 @@ def p7_probs_no_slit():
 
 
 
-def p8_three_steps():
+def p8_three_steps(h = 0.005):
     data = np.load("npz/p8.npz")
     m = int(np.sqrt(data.shape[1] - 1))
     t = data[:, 0]
@@ -79,12 +92,12 @@ def p8_three_steps():
     real = np.load("npz/p8_real.npz")
     imag = np.load("npz/p8_imag.npz")
 
-    axes = np.linspace(0, 1, m)
+    axes = np.linspace(h, 1-h, m)
 
     times = [0.0, 0.001, 0.002]
 
     for time in times:
-        fig = go.Figure(data=go.Contour(x=axes, y=axes, z=p[np.argmin(abs(t - time))]))
+        fig = go.Figure(data=go.Contour(x=axes, y=axes, z=p[time_to_index(time)]))
         fig.update_layout(
             font_family="Garamond",
             font_size=30,
@@ -97,7 +110,7 @@ def p8_three_steps():
 
     for time in times:
         for name, part in zip(["Imaginary", "Real"], [imag, real]):
-            fig = go.Figure(data=go.Contour(x=axes, y=axes, z=part[np.argmin(abs(t - time))]))
+            fig = go.Figure(data=go.Contour(x=axes, y=axes, z=part[time_to_index(time)]))
             fig.update_layout(
                 font_family="Garamond",
                 font_size=30,
@@ -108,11 +121,46 @@ def p8_three_steps():
             fig.show()
 
 
+def p9(slits, h = 0.005):
+    #data = np.load(f"npz/p9_{slits}_slits.npz")
+    data = np.load("npz/p7_no_slit_final.npz")
+    m = int(np.sqrt(data.shape[1] - 1))
+    t = data[:, 0]
 
-def plot_potential(slits):  #Just for testing what the potential looks like
+    print(t[0])
+    print(t[-1])
+
+    t_index = time_to_index(0.002)
+
+    p = data[:, 2:].reshape(len(t), m, m)
+    x_index = position_to_index(0.8)
+
+    screen = p[t_index, x_index, :]
+
+    norm = np.sum(screen)
+    screen /= norm
+
+    axis = np.linspace(h, 1-h, m)
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x = axis, y = screen, mode="lines", line=dict(width = 5)))
+    fig.update_layout(
+        font_family="Garamond",
+        font_size=30,
+        title = "Probability distribution along y-axis at x = 0.8, t = 0.002",
+        xaxis_title= "Position along y-axis",
+        yaxis_title= "Probability")
+    fig.show()
+
+
+
+
+
+
+def plot_potential(slits, h = 0.005):  #Just for testing what the potential looks like
     V = np.load(f"npz/potential_{slits}_slits.npz")
     m = V.shape[0]
-    x = np.linspace(0, 1, m)
+    x = np.linspace(h, 1-h, m)
     fig = go.Figure(go.Contour(x=x, y=x, z=V))
     fig.show()
 
@@ -124,6 +172,7 @@ if __name__ == "__main__":
     #p7_probs_no_slit()
     #p8_three_steps()
     #animate()
-    plot_potential(2)
-    # plot_states()
+    #plot_potential(2)
+    #plot_states()
+    p9(2)
     # plot_initial_u()
