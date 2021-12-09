695968c6bc6095247b1176a45d5a9ee9dfcf70f5
haakooto <hakonolav.torvik@gmail.com>
2021-12-09 12:09:06 +0100

    do some more tests

--- Project5/tests.jl
+++ Project5/tests.jl
@@ -1,6 +1,8 @@
 using Random
 using LinearAlgebra
+using NPZ
 include("sparse_matrices.jl")
+include("Crank_Nicolson.jl")
 
 
 function test_multiplication()
@@ -54,32 +56,65 @@ end
 function determine_best_omega()
     rng = MersenneTwister(14)
 
-    n = 10 ^ 2
-    r = 1im / 2
-    da = ones(n) .+ 4r
-    db = ones(n) .- 4r
-
-    A = SparseMat(da, -r)
-    B = SparseMat(db, r)
+    h = 0.01
+    dt = h^2
+    V = make_V(h, 0, 2)
+    A, B = make_AB(h, dt, V)
+    n = size(A.diag)[1]
 
     timesteps = 20
 
-    ws = collect(range(0.1, 1.9, step=0.1))
+    ws = collect(range(0.7, 1.1, step=0.1))
     is = similar(ws)
 
     for k in 1:length(ws)
+        println("omega = ", ws[k])
         x = randn(rng, ComplexF64, n)
         i_ = zeros(timesteps)
         for t in 1:timesteps
             b = B * x
             x, i_[t] = SOR(A, b, omega=ws[k], initial_guess=x)
+            print(i_[t], " ")
         end
         is[k] = sum(i_[2:end]) / length(i_[2:end])
+        println("Average: ", is[k])
     end
     println("For M = ", sqrt(n), ", the omega giving fewest SOR-iterations over ", timesteps, " was ", ws[argmin(is)], " averaging ", minimum(is), " iterations per SOR")
 end
 
-test_multiplication()
-test_SOR()
-determine_best_omega()
+function test_sparse()
+    h = 0.005
+    dt = h^2
+    V = make_V(h, 0, 2)
+    A, B = make_AB(h, dt, V)
+    # A_ds = to_dense(A)
+    A_sp = to_sparse(A)
+
+    n = size(A.diag)[1]
+    println(sqrt(n))
+    b = randn(ComplexF64, n)
+
+    @time x_or, i = SOR(A, b, omega=0.9)
+    # @time x_ds = inv(A_ds) * b
+    @time x_sp = A_sp \ b
+    @time x_spor, j = SOR(A_sp, b, omega=0.9)
+
+    println("iterations: ", i)
+    println("iterations: ", j)
+    # r = sum(x_or .- x_ds)
+    # s = sum(x_ds .- x_sp)
+    t = sum(x_sp .- x_or)
+    r = sum(x_spor .- x_or)
+    s = sum(x_spor .- x_sp)
+
+    println(r)
+    println(s)
+    println(t)
+
+end
+
+# test_multiplication()
+# test_SOR()
+# determine_best_omega()
+test_sparse()
 
